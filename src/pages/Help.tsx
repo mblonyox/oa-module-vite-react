f2d64f2af0b6faff1a221bbd45ff91c65579e3d0
Sukirno <mblonyox@gmail.com>
2021-10-22 14:10:50 +0700

    fix: sample content

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,25 @@
+import { Paper, Typography } from "@mui/material";
 import BasePage from "../components/BasePage";
 
 function Help() {
-  return <BasePage title="Help"></BasePage>;
+  return (
+    <BasePage title="Help">
+      <Paper
+        sx={{
+          m: 2,
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: 240,
+        }}
+      >
+        <Typography variant="h5">{import.meta.env.VITE_APP_TITLE}</Typography>
+        <Typography variant="subtitle1">Versi 1.0.0</Typography>
+      </Paper>
+    </BasePage>
+  );
 }
 
 export default Help;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { Paper, Typography } from "@mui/material";
 import BasePage from "../components/BasePage";
 
 function Home() {
-  return <BasePage title="Home">
-  </BasePage>;
+  return (
+    <BasePage title="Home">
+      <Paper
+        sx={{
+          m: 2,
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: 240,
+        }}
+      >
+        <Typography variant="h5">
+          Welcome to {import.meta.env.VITE_APP_TITLE}
+        </Typography>
+      </Paper>
+    </BasePage>
+  );
 }
 
 export default Home;
